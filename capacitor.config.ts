636f91c728e3c6d0420353a5034176b758d0d1fb
HernanJMF <97522680+HernanJMF@users.noreply.github.com>
2025-05-30 15:32:59 -0500

    UPDATE: app name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'es.agroseguro.chatbot',
-  appName: 'Agroseguro Chatbot',
+  appName: 'Agroseguro documental',
   webDir: 'dist',
   plugins: {
     SplashScreen: {
